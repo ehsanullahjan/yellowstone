3bf9c863bb5f24646d593c498548e005c3ebcf17
Podman <podman@example.com>
2026-07-09 02:21:45 -0400

    Add tailscale service

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,4 +10,5 @@ rsync -rvK /ctx/system_files/ /
 /ctx/build_files/install/docker.sh
 /ctx/build_files/install/podman.sh
 /ctx/build_files/install/libvirt.sh
+/ctx/build_files/install/tailscale.sh
 
---
+++ build_files/install/tailscale.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpm --import https://pkgs.tailscale.com/stable/fedora/repo.gpg
+cat <<-EOF >/etc/yum.repos.d/tailscale.repo
+	[tailscale-stable]
+	name=Tailscale stable
+	baseurl=https://pkgs.tailscale.com/stable/fedora/\$basearch
+	enabled=1
+	repo_gpgcheck=1
+	gpgcheck=1
+	gpgkey=https://pkgs.tailscale.com/stable/fedora/repo.gpg
+EOF
+dnf config-manager setopt tailscale-stable.enabled=0
+
+dnf -y --enable-repo=tailscale-stable install tailscale
+systemctl enable tailscaled.service
