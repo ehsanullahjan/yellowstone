fb2f93298d4f31e34311cd1010677cd7b77eb18e
Podman <podman@example.com>
2026-07-09 16:56:48 -0400

    Add bitmap fonts suitable for a headless env

--- build_files/install/fonts.sh
+++ build_files/install/fonts.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
 set -ouex pipefail
 
-dnf -y copr enable che/nerd-fonts
-dnf config-manager setopt copr:copr.fedorainfracloud.org:che:nerd-fonts.enabled=0
-
-dnf -y --enable-repo=copr:copr.fedorainfracloud.org:che:nerd-fonts install \
-	cascadia-code-fonts \
-	fira-code-fonts \
-	jetbrains-mono-fonts-all \
-	nerd-fonts
+INCLUDED_PACKAGES=(
+	terminus-fonts-console
+)
+dnf -y install "${INCLUDED_PACKAGES[@]}"
