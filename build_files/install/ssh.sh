61aaf96f76040fda1431c22534ef2c44fc6e51eb
Podman <podman@example.com>
2026-07-16 22:04:31 -0400

    Move ssh-related packages to their own script

--- build_files/install/ssh.sh
+++ build_files/install/ssh.sh
@@ -3,8 +3,11 @@
 set -ouex pipefail
 
 INCLUDED_PACKAGES=(
-	NetworkManager-wifi
-	wpa_supplicant
+	et
+	mosh
+	openssh-server
 )
 dnf -y install "${INCLUDED_PACKAGES[@]}"
 
+systemctl enable sshd.service
+systemctl enable et.service
