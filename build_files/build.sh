922dc3812753c6094c05711e6284e1a5e761a0b5
Podman <podman@example.com>
2026-07-26 02:34:16 -0400

    Rename rocm.sh to amd-hwe.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 /ctx/build_files/pre-build.sh
 
-/ctx/build_files/install/rocm.sh
+/ctx/build_files/install/amd-hwe.sh
 /ctx/build_files/install/rpmfusion.sh
 
 /ctx/build_files/install/firewall.sh
