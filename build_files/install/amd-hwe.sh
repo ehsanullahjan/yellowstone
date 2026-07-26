fa8c6f73e8539178da93684aee59e8170454d9a3
Podman <podman@example.com>
2026-07-26 02:51:48 -0400

    Replace rpmfusion's mesa build with terra's
    Install mesa from terra instead of rpmfusion for a more complete build.
    Terra's mesa build has patches from the Open Game Collective and enables
    additional features that are disabled in rpmfusion and fedora's builds.

--- build_files/install/amd-hwe.sh
+++ build_files/install/amd-hwe.sh
@@ -3,3 +3,4 @@
 set -euxo pipefail
 
 dnf -y install rocm-smi rocminfo
+dnf -y install libva-utils mesa-va-drivers
--- build_files/install/rpmfusion.sh
+++ build_files/install/rpmfusion.sh
@@ -2,8 +2,6 @@
 
 set -euxo pipefail
 
-# Enable rpmfusion and its dependencies
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
 dnf -y install \
 	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
 	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
@@ -11,11 +9,7 @@ dnf -y install \
 # Enable add'l multimedia codecs
 dnf -y swap ffmpeg-free ffmpeg --allowerasing
 
-# Enable hw-accel for AMD hardware
-dnf -y install mesa-va-drivers-freeworld libva-utils
-
 dnf config-manager setopt \
-	fedora-cisco-openh264.enabled=0 \
 	rpmfusion-free.enabled=0 \
 	rpmfusion-free-updates.enabled=0 \
 	rpmfusion-nonfree.enabled=0 \
