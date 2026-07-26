1088d46c5c55b9242faf6be3f3efb13635a8dab7
Podman <podman@example.com>
2026-07-26 03:41:38 -0400

    Rename amd-hwe.sh to amd-hwer.sh
    This is more aligned with the purpose of the script to install and
    configure packages related to both hardware enablement and reporting.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 /ctx/build_files/pre-build.sh
 
-/ctx/build_files/install/amd-hwe.sh
+/ctx/build_files/install/amd-hwer.sh
 /ctx/build_files/install/multimedia.sh
 
 /ctx/build_files/install/firewall.sh
