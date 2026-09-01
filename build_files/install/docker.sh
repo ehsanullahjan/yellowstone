9fddfe25f654f10c37c756c1b7d4afb8e1fb0543
Podman <podman@example.com>
2026-09-01 00:30:59 -0400

    Drop outdated repo copr:atim:lazydocker
    lazydocker from corp:atim:lazydocker was built over two years ago, and
    no longer works with the latest Docker API. Until we find an alternative
    lazydocker repo, install the tui through distrobox.

--- build_files/install/docker.sh
+++ build_files/install/docker.sh
@@ -5,17 +5,13 @@ set -euxo pipefail
 dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 dnf config-manager setopt docker-ce-stable.enabled=0
 
-dnf copr -y enable atim/lazydocker
-dnf config-manager setopt copr:copr.fedorainfracloud.org:atim:lazydocker.enabled=0
-
-dnf -y --enable-repo=docker-ce-stable --enable-repo=copr:copr.fedorainfracloud.org:atim:lazydocker install \
+dnf -y --enable-repo=docker-ce-stable install \
 	containerd.io \
 	docker-buildx-plugin \
 	docker-compose-plugin \
 	docker-model-plugin \
 	docker-ce \
-	docker-ce-cli \
-	lazydocker
+	docker-ce-cli
 
 # Create sysusers.d entry for docker group
 cat <<-EOF >/usr/lib/sysusers.d/docker.conf
